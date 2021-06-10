b98c618ca73aff426a6e626a7c58630b2be5b9d8
Claudio-C-Santos <cula.santos@gmail.com>
2021-06-10 11:29:55 +0000

    <fix>Webhooks and checkout form

--- checkout/views.py
+++ checkout/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
 from bag.context import bag_contents
 
+import json
 import stripe
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.apy_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user, 
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry but your payment cannot be processed at the moment. Please try again later.')
+        return HttpResponse(content=e, status=400)
+
 
 
 def checkout(request):
@@ -32,9 +52,13 @@ def checkout(request):
         order_form = OrderForm(form_data)
 
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
+
             for item_id, item_data in bag.items():
-                #TODO: Make sure that item_id is an integer
                 item_id = int(item_id)
                 try:
                     product = Product.objects.get(id=item_id)
