7ff6f56368a49ff2917bd857e6727557ffdf29f7
Claudio-C-Santos <cula.santos@gmail.com>
2021-06-18 09:55:11 +0000

    <fix>bag variable in webhook_handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -22,7 +22,7 @@ class StripeWH_Handler:
 
         intent = event.data.object
         pid = intent.id
-        bag = intent.metadata.bag
+        bag = intent.charges.data[0].metadata.bag
         save_info = intent.metadata.save_info
         billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
---
+++ profiles/static/js/countryfield.js
@@ -0,0 +1,14 @@
+let countrySelected = $('#id_default_country').val();
+
+if(!countrySelected) {
+    $('#id_default_country').css('color', '#8b9299');
+};
+
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', '#8b9299');
+    } else {
+        $(this).css('color', '#000');
+    }
+});
