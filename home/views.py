bb5806c62d37b4f5000fae1d5360be3dc64b4613
Claudio-C-Santos <cula.santos@gmail.com>
2021-06-23 15:47:27 +0000

    <build>About page

--- home/views.py
+++ home/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     # View returnig index page from home
     return render(request, 'home/index.html')
+
+
+def about(request):
+    return render(request, 'home/about.html')
