0cf8cafeac49ffa60b7d53849432fe713ee9288d
Claudio-C-Santos <cula.santos@gmail.com>
2021-07-13 19:33:45 +0000

    Removed secret key and set debug

--- peacefulmind_art/settings.py
+++ peacefulmind_art/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@_3hi+3qe&s#n8dye1!3*5tw=q3(ee$g$w30r*riuzv35y=+ze'
+SECRET_KEY = 'os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['peacefulmind-art.herokuapp.com', 'localhost']
 
