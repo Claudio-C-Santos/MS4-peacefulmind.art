93f5c9ac30ca50733926190279f67c0b1d0c40ac
Claudio-C-Santos <cula.santos@gmail.com>
2021-07-24 13:20:32 +0000

    settings.py clean up

--- peacefulmind_art/settings.py
+++ peacefulmind_art/settings.py
@@ -57,24 +57,7 @@ INSTALLED_APPS = [
 
     'crispy_forms',
     'storages'
-]
-
-""" Use when implementing socia media login
-
-SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        # For each OAuth based provider, either add a ``SocialApp``
-        # (``socialaccount`` app) containing the required client
-        # credentials, or list them here:
-        'APP': {
-            'client_id': '123',
-            'secret': '456',
-            'key': ''
-        }
-    }
-}
-
-"""
+]export DATABASE_URL = 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
